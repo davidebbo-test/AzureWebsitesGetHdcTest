cfce414abeffc5d61b52355e113205aeebeaf65a
davidebbo <david.ebbo@microsoft.com>
2014-07-07 08:21:34 -0700

    Make the PDF downloadable + move code around

--- AzureWebsitesGetHdcTest/Default.aspx.cs
+++ AzureWebsitesGetHdcTest/Default.aspx.cs
@@ -1,6 +1,9 @@
 ﻿namespace AzureWebsitesGetHdcTest
 {
     using System;
+    using System.IO;
+    using ClosedXML.Excel;
+    using Microsoft.Reporting.WebForms;
 
     public partial class Default : System.Web.UI.Page
     {
@@ -10,14 +13,45 @@
 
         protected void TestClosedXML_Click(object sender, EventArgs e)
         {
-            Test.TestClosedXML();
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Sample Sheet");
+            var column = worksheet.Column(1);
+
+            worksheet.Cell("A1").Value = "Hello";
+            column.AdjustToContents();
+            var initialWidth = column.Width;
+
+            worksheet.Cell("A1").Value = "Hello World!";
+            column.AdjustToContents();
+            var width = column.Width;
+
+            if (width <= initialWidth || width > initialWidth * 10)
+            {
+                throw new Exception(string.Format("Width was not correctly adjusted. Initial: {0}, Current: {1}", initialWidth, width));
+            }
+
             Label1.Text = "The TestClosedXML test was successful";
         }
 
         protected void TestReportViewer_Click(object sender, EventArgs e)
         {
-            byte[] bytes = Test.TestReportViewer();
-            Label1.Text = String.Format("A PDF file with site {0} was successfully created", bytes.Length);
+            Microsoft.Reporting.WebForms.Warning[] warnings;
+            string[] streamIds;
+            var mimeType = string.Empty;
+            var encoding = string.Empty;
+            var extension = string.Empty;
+
+            var reportViewer = new ReportViewer();
+            reportViewer.LocalReport.LoadReportDefinition(new StreamReader(typeof(Default).Assembly.GetManifestResourceStream("AzureWebsitesGetHdcTest.simple.rdl")));
+
+            var bytes = reportViewer.LocalReport.Render("PDF", null, out mimeType, out encoding, out extension, out streamIds, out warnings);
+
+            Response.Buffer = true;
+            Response.Clear();
+            Response.ContentType = mimeType;
+            Response.AddHeader("content-disposition", "attachment; filename=" + "test");
+            Response.BinaryWrite(bytes); // create the file
+            Response.Flush(); // send it to the client to download 
         }
     }
 }
